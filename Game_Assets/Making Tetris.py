fe252beaf9d5db3252cdd726c511d501ceaeb56b
FrancisNdirangu <frankngugi@gmail.com>
2022-03-20 13:28:04 -0500

    fixing boundary cases. not fully fixed though

--- Game_Assets/Making Tetris.py
+++ Game_Assets/Making Tetris.py
@@ -13,28 +13,28 @@ background_img = pygame.image.load('C:/Users/franc/Downloads/Tetris-game-assets/
 
 #now I will place the different shapes we need for Tetris
 
-locationx = 250
+locationx = 212
 locationy = 150
 
 
 I_shape = pygame.image.load('C:/Users/franc/Downloads/Tetris-game-assets/Game_Assets/I/1.png')
-I_shape = pygame.transform.scale(I_shape,(150,150))
+I_shape = pygame.transform.scale(I_shape,(200,200))
 I_shaperect = I_shape.get_rect(midbottom = (locationx,locationy))
 
 L_shape = pygame.image.load('C:/Users/franc/Downloads/Tetris-game-assets/Game_Assets/L/L.png')
-L_shape = pygame.transform.scale(L_shape,(150,150))
+L_shape = pygame.transform.scale(L_shape,(200,200))
 L_shaperect = L_shape.get_rect(midbottom = (locationx,locationy))
 
 O_shape = pygame.image.load('C:/Users/franc/Downloads/Tetris-game-assets/Game_Assets/R/R.png')
-O_shape = pygame.transform.scale(O_shape,(150,150))
+O_shape = pygame.transform.scale(O_shape,(200,200))
 O_shaperect = O_shape.get_rect(midbottom = (locationx,locationy))
 
 Z_shape = pygame.image.load('C:/Users/franc/Downloads/Tetris-game-assets/Game_Assets/S/S.png')
-Z_shape = pygame.transform.scale(Z_shape,(150,150))
+Z_shape = pygame.transform.scale(Z_shape,(200,200))
 Z_shaperect = Z_shape.get_rect(midbottom = (locationx,locationy))
 
 T_shape = pygame.image.load('C:/Users/franc/Downloads/Tetris-game-assets/Game_Assets/T/T_2.png')
-T_shape = pygame.transform.scale(T_shape,(150,150))
+T_shape = pygame.transform.scale(T_shape,(200,200))
 T_shaperect = T_shape.get_rect(midbottom = (locationx,locationy))
 
 shape_chosen = [I_shape,L_shape,O_shape,Z_shape,T_shape]
@@ -46,19 +46,12 @@ print(index_shape)
 
 running = True
 
-location_tracker_horizontal = 250
+location_tracker_horizontal = 212 #tracking the horizontal position of the shape
 while running:
     pygame.display.flip()
     clock.tick(60)
     screen.blit(background_img,(0,0))
-    # locationy += 2
-    # if locationy > 810:
-    #     locationy = 810
-    # I_shaperect.bottom += 2
-    # L_shaperect.bottom += 2
-    # O_shaperect.bottom += 2
-    # Z_shaperect.bottom += 2
-    # T_shaperect.bottom += 2
+
     for i in range(len(shape_chosenrect)):
         shape_chosenrect[i].bottom +=2
         if shape_chosenrect[i].bottom > 990:
@@ -81,18 +74,18 @@ while running:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
                 shape_chosenrect[index_shape].right += 10
-                location_tracker_horizontal += 10
-                if location_tracker_horizontal >400:
-                    location_tracker_horizontal = 400
-                if shape_chosenrect[index_shape].right > 470:
-                    shape_chosenrect[index_shape].right = 470
+                location_tracker_horizontal = shape_chosenrect[index_shape].right
+                # if location_tracker_horizontal >400:
+                #     location_tracker_horizontal = 400 #setting a limit on the location tracker to know boundary has been reached
+                if shape_chosenrect[index_shape].right > 465:
+                    shape_chosenrect[index_shape].right = 465
             if event.key == pygame.K_LEFT:
                 shape_chosenrect[index_shape].left -= 10
                 location_tracker_horizontal -= 10
-                if location_tracker_horizontal < 50:
-                    location_tracker_horizontal = 50
-                if shape_chosenrect[index_shape].left < -15:
-                    shape_chosenrect[index_shape].left = -15
+                # if location_tracker_horizontal < -50:
+                #     location_tracker_horizontal = -50 #setting a limit on the location tracker to know boundary has been reached
+                if shape_chosenrect[index_shape].left < -50:
+                    shape_chosenrect[index_shape].left = -50
             if event.key == pygame.K_UP: #this code doesnt work. the reason is because we set shape_chosen[index_shape].bottom to a constant. the bottom must change as the shape rotates for this to work
                 shape_chosen[index_shape] = pygame.transform.rotate(shape_chosen[index_shape],90) #the code works when i only rotate the image
                 #shape_chosenrect[index_shape] = pygame.transform.rotate(shape_chosenrect[index_shape],90) the code doesnt work when i try to rotate the rectangle surface as well
@@ -103,7 +96,8 @@ while running:
         #i can then also make another if statement to produce new shapes when the "floor" is reached since I dont know how to make 
     
 
-
+#some of the shapes dont reach the left or right boundary because they have different thicknesses
+#find a way of using collisions to make all the shapes able to reach the edge
     
 
 pygame.quit()
