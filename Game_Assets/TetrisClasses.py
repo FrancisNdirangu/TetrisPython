ceb64d4af2bf3f662faef0d813ea5d46f7736b52
FrancisNdirangu <frankngugi@gmail.com>
2022-04-10 13:07:09 -0500

    correcting the shape file paths

--- Game_Assets/TetrisClasses.py
+++ Game_Assets/TetrisClasses.py
@@ -1,6 +1,6 @@
 #TetrisClasses
 
-from curses import KEY_LEFT, KEY_RIGHT
+
 import pygame
 import random
 pygame.init()
@@ -21,14 +21,14 @@ class MovingObjects(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
         #the different shapes
-        self.T_shape = pygame.image.load('C:/Users/franc/Downloads/Tetris-game-assets/Game_Assets/T/T_2.png')
-        self.Z_shape = pygame.image.load('C:/Users/franc/Downloads/Tetris-game-assets/Game_Assets/S/S.png')
-        self.O_shape = pygame.image.load('C:/Users/franc/Downloads/Tetris-game-assets/Game_Assets/R/R.png')
-        self.L_shape = pygame.image.load('C:/Users/franc/Downloads/Tetris-game-assets/Game_Assets/L/L.png')
-        self.I_shape = pygame.image.load('C:/Users/franc/Downloads/Tetris-game-assets/Game_Assets/I/1.png')
+        T_shape = pygame.image.load('C:/Users/franc/Downloads/Tetris-game-assets/Game_Assets/T/T_2.png')
+        Z_shape = pygame.image.load('C:/Users/franc/Downloads/Tetris-game-assets/Game_Assets/S/S.png')
+        O_shape = pygame.image.load('C:/Users/franc/Downloads/Tetris-game-assets/Game_Assets/R/R.png')
+        L_shape = pygame.image.load('C:/Users/franc/Downloads/Tetris-game-assets/Game_Assets/L/L.png')
+        I_shape = pygame.image.load('C:/Users/franc/Downloads/Tetris-game-assets/Game_Assets/I/1.png')
 
         #array full of the shapes
-        self.shapes = [self.T_shape,self.Z_shape,self.O_shape,self.L_shape,self.I_shape]
+        self.shapes = [T_shape,Z_shape,O_shape,L_shape,I_shape]
         #this will be the shape that we are controlling
         self.shape_chosen = self.shapes[random.randint(0,len(self.shapes))]
     
